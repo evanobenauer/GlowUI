ee4d742963c2e69c3fd009095d4031093dfc0b4a
Evan Obenauer <evanjohnobenauer@gmail.com>
2023-10-08 15:04:35 -0400

    Fixed issue causing RGB channels to have B and R swapped in Icon setting

--- src/main/java/com/ejo/glowui/Window.java
+++ src/main/java/com/ejo/glowui/Window.java
@@ -354,7 +354,7 @@ public class Window {
 
 
     public static GLFWImage.Buffer getImageBuffer(File imageFile, int width, int height) {
-        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_BGR);
         Graphics2D graphics = img.createGraphics();
         try {
             graphics.scale((double) img.getWidth() / ImageIO.read(imageFile).getWidth(),(double) img.getHeight() / ImageIO.read(imageFile).getHeight());
@@ -363,7 +363,7 @@ public class Window {
             e.printStackTrace();
         }
 
-        DataBuffer dataBuffer = img.getRaster().getDataBuffer();
+        DataBuffer dataBuffer = img.getRaster().createWritableChild(0, 0, img.getWidth(), img.getHeight(), 0, 0, new int[]{2, 1, 0}).getDataBuffer();
         int[] imageData = ((DataBufferInt) dataBuffer).getData();
         ByteBuffer buffer = BufferUtils.createByteBuffer(imageData.length * 16);
         for (int pixel : imageData) buffer.putInt(pixel);
