0331ed73515c8d621408d2a42a7d60b265a1a6de
Evan Obenauer <evanjohnobenauer@gmail.com>
2023-10-27 11:05:02 -0400

    Combined CircleUI and SemiCircleUI into 1 class

--- src/main/java/com/ejo/glowui/scene/elements/shape/CircleUI.java
+++ src/main/java/com/ejo/glowui/scene/elements/shape/CircleUI.java
@@ -1,5 +1,7 @@
 package com.ejo.glowui.scene.elements.shape;
 
+import com.ejo.glowlib.math.Angle;
+import com.ejo.glowlib.math.MathE;
 import com.ejo.glowlib.math.Vector;
 import com.ejo.glowlib.misc.ColorE;
 import com.ejo.glowui.scene.Scene;
@@ -8,21 +10,33 @@ import org.lwjgl.opengl.GL11;
 
 public class CircleUI extends ElementUI implements IShape {
 
+    private static final Angle FULL = new Angle(360,true);
+
     private ColorE color;
     private boolean outlined;
     private double radius;
+    private Angle range;
     private Type type;
 
-    public CircleUI(Vector pos, ColorE color, boolean outlined, double radius, Type type) {
+    public CircleUI(Vector pos, ColorE color, boolean outlined, double radius, Angle range, Type type) {
         super(pos,true,true);
         this.color = color;
         this.outlined = outlined;
         this.radius = radius;
+        this.range = range;
         this.type = type;
     }
 
+    public CircleUI(Vector pos, ColorE color, double radius, Angle range, CircleUI.Type type) {
+        this(pos,color,false,radius,range,type);
+    }
+
+    public CircleUI(Vector pos, ColorE color, boolean outlined, double radius, CircleUI.Type type) {
+        this(pos,color,outlined,radius,FULL,type);
+    }
+
     public CircleUI(Vector pos, ColorE color, double radius, Type type) {
-        this(pos,color,false,radius,type);
+        this(pos,color,false,radius,FULL,type);
     }
 
     @Override
@@ -30,7 +44,8 @@ public class CircleUI extends ElementUI implements IShape {
         GL11.glColor4f(getColor().getRed() / 255f, getColor().getGreen() / 255f, getColor().getBlue() / 255f, getColor().getAlpha() / 255f);
         GL11.glDisable(GL11.GL_LINE_STIPPLE);
         GL11.glBegin(isOutlined() ? GL11.GL_LINE_LOOP : GL11.GL_POLYGON);
-        double radianIncrement = 2*Math.PI / getType().getVertices();
+        double radianIncrement = getRange().getRadians() / getType().getVertices();
+        if (!getRange().equals(FULL)) GL11.glVertex2f((float) getPos().getX(), (float) getPos().getY()); //Center Vertex
         for (int i = 0; i <= getType().getVertices(); i++) {
             Vector vert = new Vector(Math.cos(radianIncrement*i),Math.sin(radianIncrement*i)).getMultiplied(getRadius());
             GL11.glVertex2f((float) getPos().getX() + (float) vert.getX(), (float) getPos().getY() + (float) vert.getY());
@@ -45,7 +60,12 @@ public class CircleUI extends ElementUI implements IShape {
 
     @Override
     public boolean updateMouseOver(Vector mousePos) {
-        return mousePos.getAdded(getCenter().getMultiplied(-1)).getMagnitude() < getRadius();
+        Vector relativeMousePos = mousePos.getSubtracted(getCenter());
+        if (getRadius() < relativeMousePos.getMagnitude()) return false;
+        if (getRange().equals(FULL)) return true;
+
+        if (relativeMousePos.getY() > 0) return relativeMousePos.getTheta().getDegrees() <= getRange().getDegrees();
+        else return relativeMousePos.getTheta().getDegrees() >= getRange().getDegrees();
     }
 
 
@@ -65,6 +85,10 @@ public class CircleUI extends ElementUI implements IShape {
 
     public void setRadius(double radius) {
         this.radius = radius;
+    }
+
+    public void setRange(Angle range) {
+        this.range = range;
     }
 
     public void setType(Type type) {
@@ -88,6 +112,10 @@ public class CircleUI extends ElementUI implements IShape {
 
     public double getRadius() {
         return radius;
+    }
+
+    public Angle getRange() {
+        return range;
     }
 
     public Type getType() {
